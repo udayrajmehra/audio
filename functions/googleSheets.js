d7c1627e7c4ceff4cf68338cc29587c4e9471cf2
udaym1 <udaymehra2@gmail.com>
2023-06-14 16:32:43 -0700

    updated serverless function with CORS perms

--- functions/googleSheets.js
+++ functions/googleSheets.js
@@ -1,16 +1,22 @@
 const fetch = require('node-fetch');
 const Papa = require('papaparse');
+const express = require('express');
+const cors = require('cors');
 
 function parseCSV(csvData) {
-    const parsedData = Papa.parse(csvData, { header: true }).data;
-    console.log('Parsed data:', parsedData);
-    return parsedData;
-  }
-  
+  const parsedData = Papa.parse(csvData, { header: true }).data;
+  console.log('Parsed data:', parsedData);
+  return parsedData;
+}
 
-exports.handler = async (event, context) => {
+const app = express();
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
   try {
-    const googleSheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_Oq8Do15RWSHJ5kUhmRET7E5Lw7wCxOByJtmXQ-ACs4DEPZVATqi-rCwX7COgibDaI06qUnbutDP1/pub?gid=0&single=true&output=csv';
+    const googleSheetURL =
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_Oq8Do15RWSHJ5kUhmRET7E5Lw7wCxOByJtmXQ-ACs4DEPZVATqi-rCwX7COgibDaI06qUnbutDP1/pub?gid=0&single=true&output=csv';
     const response = await fetch(googleSheetURL);
     const csvData = await response.text();
 
@@ -19,10 +25,10 @@ exports.handler = async (event, context) => {
 
     const trackOrders = {
       'Studio Discography': [],
-      'Live Sessions': []
+      'Live Sessions': [],
     };
 
-    data.forEach(entry => {
+    data.forEach((entry) => {
       const trackName = entry.trackname;
       const metadata = entry.metadata;
       const spotifyURI = entry.spotifyuri;
@@ -45,23 +51,25 @@ exports.handler = async (event, context) => {
         youtubeTrackID,
         recorded,
         mixed,
-        mastered
+        mastered,
       });
     });
 
-    Object.values(trackOrders).forEach(section => {
+    Object.values(trackOrders).forEach((section) => {
       section.sort((a, b) => a.order - b.order);
     });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(trackOrders),
-    };
+    // Set the response headers
+    res.setHeader('Access-Control-Allow-Origin', 'https://udayrajmehra.github.io');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Return the response
+    res.status(200).json(trackOrders);
   } catch (error) {
     console.error('Error fetching track data:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch track data' }),
-    };
+    res.status(500).json({ error: 'Failed to fetch track data' });
   }
-};
+});
+
+module.exports = app;
