26f66291a4210378044a8c5847ca2795434ae41a
udaym1 <udaymehra2@gmail.com>
2023-06-17 16:53:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -144,11 +144,6 @@ function addTrackToWebsite(track) {
                     <img src="resources/spotify-icon.png" alt="Spotify Icon">
                 </a>
                 ` : '')}
-                ${metadata === 'song' && youtubeTrackID !== 'null' ? `
-                <a href="#" class="youtube-icon" data-video="${youtubeTrackID}" data-type="${metadata}">
-                    <img src="resources/youtube-icon.png" alt="YouTube Icon">
-                </a>
-                ` : ''}
                 ${metadata === 'album' && youtubeTrackID !== 'null' ? `
                 <a href="#" class="youtube-album-icon" data-playlist="${youtubeTrackID}" data-type="${metadata}">
                     <img src="resources/youtube-icon.png" alt="YouTube Icon">
